d475b70dc3c626964ecadaa56540fa07b3f6ccf2
Artur Dorfman <artur@pop-os.localdomain>
2020-12-10 13:10:27 +0200

    last bug fix

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		dots: true,
 	});
 
-	if ($(window).width() <= 768) {
+	function enableSlickSliders() {
 		$('.services__wrap').slick({
 			autoplay: true,
 			utoplaySpeed: 3000,
@@ -23,19 +23,26 @@ $(document).ready(function () {
 			slidesToShow: 3,
 			slidesToScroll: 3
 		});
-
 	}
+	// window resize 
+	$(window).resize(function () {
+		if ($(window).width() < 768) {
+			enableSlickSliders();
+		}
+	});
 
 	// ============== bureger=================
 	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
+		$('.header__links').click(function (event) {
+			$('.header__burger, .header__menu').removeClass('active');
+		})
 	});
 	// =======================================
 
 	// ===============popup===================
 	$('.img-popup img, .video-wrapper iframe').magnificPopup({
-		// type: 'image',
 		items: {
 			src: 'https://www.youtube.com/embed/tgbNymZ7vqY'
 		},
@@ -55,4 +62,11 @@ $(document).ready(function () {
 			srcAction: 'iframe_src',
 		}
 	});
+
+	// cansel restart page when submiting form
+	$('#form-submit').click(function (envent) {
+		event.preventDefault();
+	});
+
+
 });
